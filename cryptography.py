476c9254894794c3abca065ef76626653e8cd63d
johari2020 <42872004+johari2020@users.noreply.github.com>
2018-10-17 10:53:23 -0400

    Updated from Brython Server: 10/17/2018 10:53:22 AM

--- cryptography.py
+++ cryptography.py
@@ -21,13 +21,16 @@ while e!= "q":
     k = input("Key: ") 
     for j in m:
         n = associations.find(j)
+        print(n)
         for y in k:
             kk= associations.find(y)
             ll.append(kk)
+            print(kk)
         l.append(n)
     for f in ll:
+        print 
         o = [x+ll[h] for x in l]
-        print(o)
+        
         h+=1
     
     
@@ -40,3 +43,37 @@ while e!= "q":
     '''
     if m == "q":
         print("Goodbye!")
+
+"""
+cryptography.py
+Author: <your name here>
+Credit: <list sources used, if any>
+
+Assignment:
+
+Write and submit a program that encrypts and decrypts user data.
+
+See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
+
+associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+l=[]
+ll = []
+h=0
+e = " " 
+while e!= "q": 
+    e = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    if e == "q":
+        print("Goodbye!")
+    else:
+        m = input("Message: ")
+        k = input("Key: ") 
+        for j in m:
+            n = (associations.find(j)+1)
+            l.append(n)
+        for y in k:
+            kk= (associations.find(y)+1)
+            ll.append(kk)
+        print(ll)
+        print(l)
+"""
