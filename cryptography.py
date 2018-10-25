62f6228aad77ccde7ad909680e9c0aed8b39b095
johari2020 <42872004+johari2020@users.noreply.github.com>
2018-10-25 13:19:02 -0400

    Updated from Brython Server: 10/25/2018 1:19:01 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Johari
+Credit:megsnyder, Noah
 
 Assignment:
 
@@ -107,36 +107,3 @@ while e!= "q":
     else: 
         print("try again") 
 
-"""
-cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
-
-Assignment:
-
-Write and submit a program that encrypts and decrypts user data.
-
-See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
-
-associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-
-l=[]
-ll = []
-h=0
-e = " " 
-while e!= "q": 
-    e = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-    if e == "q":
-        print("Goodbye!")
-    else:
-        m = input("Message: ")
-        k = input("Key: ") 
-        for j in m:
-            n = (associations.find(j)+1)
-            l.append(n)
-        for y in k:
-            kk= (associations.find(y)+1)
-            ll.append(kk)
-        print(ll)
-        print(l)
-"""
